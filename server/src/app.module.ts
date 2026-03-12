6837af1cb60924acdd7a3e64d37f8eed2f9824ec
Abrham Belayneh <abrsh6266@gmail.com>
2026-03-12 14:32:37 +0300

    feat: module imported on the global app module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from "@nestjs/common";
 import { PrismaModule } from "./prisma/prisma.module";
+import { TaskModule } from "./task/task.module";
+import { SessionModule } from "./session/session.module";
+import { TimerModule } from "./timer/timer.module";
 
 @Module({
   imports: [
     PrismaModule,
+    TaskModule,
+    SessionModule,
+    TimerModule,
   ],
 })
 export class AppModule {}
